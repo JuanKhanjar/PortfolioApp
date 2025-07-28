c98a0208ff17ef9e4f0db756d23849123146bccb
Juan Khanjar <jan4ma@gmail.com>
2025-07-28 09:12:20 +0200

    Add Paeges

---
+++ src/PortfolioApp.Web/Pages/Admin/Messages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioApp.Web.Pages.Admin.Messages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
